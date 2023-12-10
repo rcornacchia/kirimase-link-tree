88fec3f952108e1d715833bc79786d21df934b25
Robert Cornacchia <robcorn75@gmail.com>
2023-12-10 10:18:42 -0500

    db migration for page_links

--- src/lib/db/schema/pageLinks.ts
+++ src/lib/db/schema/pageLinks.ts
@@ -1,35 +1,38 @@
 import { varchar, integer, serial, pgTable } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
-import { pages } from "./pages"
+import { pages } from "./pages";
 import { users } from "@/lib/db/schema/auth";
 import { getPageLinks } from "@/lib/api/pageLinks/queries";
 
-export const pageLinks = pgTable('page_links', {
+export const pageLinks = pgTable("page_links", {
   id: serial("id").primaryKey(),
   title: varchar("title", { length: 256 }).notNull(),
   url: varchar("url", { length: 256 }).notNull(),
-  pageId: integer("page_id").references(() => pages.id, { onDelete: "cascade" }).notNull(),
-  userId: varchar("user_id", { length: 256 }).references(() => users.id, { onDelete: "cascade" }).notNull(),
+  pageId: integer("page_id")
+    .references(() => pages.id, { onDelete: "cascade" })
+    .notNull(),
+  userId: varchar("user_id", { length: 256 })
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
 });
-
 
 // Schema for pageLinks - used to validate API requests
 export const insertPageLinkSchema = createInsertSchema(pageLinks);
 
 export const insertPageLinkParams = createSelectSchema(pageLinks, {
-  pageId: z.coerce.number()
-}).omit({ 
+  pageId: z.coerce.number(),
+}).omit({
   id: true,
-  userId: true
+  userId: true,
 });
 
 export const updatePageLinkSchema = createSelectSchema(pageLinks);
 
-export const updatePageLinkParams = createSelectSchema(pageLinks,{
-  pageId: z.coerce.number()
-}).omit({ 
-  userId: true
+export const updatePageLinkParams = createSelectSchema(pageLinks, {
+  pageId: z.coerce.number(),
+}).omit({
+  userId: true,
 });
 
 export const pageLinkIdSchema = updatePageLinkSchema.pick({ id: true });
@@ -40,7 +43,8 @@ export type NewPageLink = z.infer<typeof insertPageLinkSchema>;
 export type NewPageLinkParams = z.infer<typeof insertPageLinkParams>;
 export type UpdatePageLinkParams = z.infer<typeof updatePageLinkParams>;
 export type PageLinkId = z.infer<typeof pageLinkIdSchema>["id"];
-    
-// this type infers the return from getPageLinks() - meaning it will include any joins
-export type CompletePageLink = Awaited<ReturnType<typeof getPageLinks>>["pageLinks"][number];
 
+// this type infers the return from getPageLinks() - meaning it will include any joins
+export type CompletePageLink = Awaited<
+  ReturnType<typeof getPageLinks>
+>["pageLinks"][number];
